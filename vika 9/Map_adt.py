fdfe267349e30a89e6907f223296bc2b5f0ad09b
Benjamín Ragnarsson <benjarag7@outlook.com>
2024-03-12 14:26:13 +0000

    added mycomparablekey class 10%

---
+++ vika 9/Map_adt.py
@@ -0,0 +1,31 @@
+class BT_node:
+    def __init__(self, key=None, data=None, left=None, right=None):
+        self.key = key
+        self.data = data
+        self.left = left
+        self.right = right
+
+class Map_ADT:
+    def insert(key, value):
+        '''
+        ■ Adds an item to the map with this key and data value
+        '''
+        pass
+    
+    def find(key):
+        '''
+        ■ Returns the data value associated with that key
+        '''
+        pass
+
+    def update(key, value):
+        '''
+        ■ Sets a new value for the data associated with that key
+        '''
+        pass
+
+    def remove(key):
+        '''
+        ■ Removes the item associated with that key from the map
+        '''
+        pass
