4bcc2a543f714c0598caeff48b74bc722a1c59b2
Benjamín Ragnarsson <benjarag7@outlook.com>
2024-04-07 14:34:20 +0000

    bugfix

--- vika_12/PA6.py
+++ vika_12/PA6.py
@@ -4,7 +4,7 @@ def pick_a_random_word():
     '''
     Picks a random word from the word_bank.txt file
     '''
-    with open('word_bank.txt', 'r') as f:
+    with open("vika_12/word_bank.txt", "r") as f:
         words = f.readlines()
         line_words = random.choice(words)
         word_list = line_words.split(" ")
